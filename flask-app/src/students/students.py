a416cd7b92b4ed262bcf0ac45184d6866f099ae5
hhl04 <lehoangtuonghuy2004@gmail.com>
2023-12-05 23:16:20 -0500

    student and  user interactions

--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -45,3 +45,85 @@ def get_student_detail (student_id):
     for row in the_data:
         json_data.append(dict(zip(column_headers, row)))
     return jsonify(json_data)
+
+@students.route('/students', methods=['POST'])
+def add_new_dispute():
+
+    # collecting data from the request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    student_id = random.randint(0,999999999)
+    name = the_data['Name']
+    email = the_data['Email']
+    major = the_data['Major']
+    grade_year = the_data['GradeYear']
+    num_coops = the_data['NumCoops']
+
+
+    # Constructing the query
+    query = 'insert into Student (Studentid, Name, Email, Major, GradeYear, NumCoops) values ('
+    query += str(student_id) + ', '
+    query += '"' + name + '", '
+    query += '"' + email + '", '
+    query += '"' + major + '", '
+    query += '"' + grade_year + '", '
+    query += str(num_coops) + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+
+@students.route('/students/<student_id>', methods=['PUT'])
+def update_guideline(student_id):
+
+    # collecting data from the request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    student_id = random.randint(0,999999999)
+    name = the_data['Name']
+    email = the_data['Email']
+    major = the_data['Major']
+    grade_year = the_data['GradeYear']
+    num_coops = the_data['NumCoops']
+
+    # Constructing the query
+    query = 'update Student set '
+
+    query += 'Name = ' + '"' + name + '", '
+    query += 'Email = ' + '"' + email + '", ' 
+    query += 'Major = ' + '"' + major + '", ' 
+    query += 'GradeYear = ' + '"' + grade_year + '", ' 
+    query += 'NumCoop = ' + str(num_coops) + '"'
+
+    query += 'where StudentID = '
+    query += str(student_id)
+    query += ";"
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+
+@students.route('/students/<student_id>', methods=['DELETE'])
+def delete_guideline (student_id):
+
+    query = 'DELETE FROM Guidelines WHERE StudentID = ' + str(student_id)
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
--- flask-app/src/user_interactions/user_interactions.py
+++ flask-app/src/user_interactions/user_interactions.py
@@ -64,7 +64,7 @@ def get_user_interactions_detail_student_id (student_id):
 @user_interactions.route('/user_interactions/interaction_id/<interaction_id>', methods=['GET'])
 def get_user_interactions_detail_interaction_id (interaction_id):
 
-    query = 'SELECT ModeratorId, StudentID, InteractionID, UserTimestamp, Content, Status FROM User_Interaction WHERE InteractionID = ' + str(interaction_id)
+    query = 'SELECT ModeratorID, StudentID, InteractionID, UserTimestamp, Content, Status FROM User_Interaction WHERE InteractionID = ' + str(interaction_id)
     current_app.logger.info(query)
 
     cursor = db.get_db().cursor()
@@ -75,3 +75,91 @@ def get_user_interactions_detail_interaction_id (interaction_id):
     for row in the_data:
         json_data.append(dict(zip(column_headers, row)))
     return jsonify(json_data)
+
+@user_interactions.route('/user_interactions', methods=['POST'])
+def add_new_user_interaction():
+#ModeratorId, StudentID, InteractionID, UserTimestamp, Content, Status 
+    # collecting data from the request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    moderator_id = the_data['ModeratorID']
+    student_id = the_data['StudentID']
+    interaction_id = random.randint(0,999999999)
+    user_timestamp = the_data['UserTimestamp']
+    content = the_data['Content']
+    status = random.randint(0,1)
+
+
+    # Constructing the query
+    query = 'insert into User_Interaction (ModeratorID, StudentID, InteractionID, UserTimestamp, Content, Status) values ('
+
+    query += str(moderator_id) + ', '
+    
+    query += str(student_id) + ', '
+    query += str(interaction_id) + ', '
+    query += '"' + user_timestamp + '", '
+    query += '"' + content + '", '
+    query += str(status) + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+
+@user_interactions.route('/user_interactions/interaction_id/<interaction_id>', methods=['PUT'])
+def update_guideline(student_id):
+
+    # collecting data from the request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    moderator_id = the_data['ModeratorId']
+    student_id = the_data['StudentID']
+    interaction_id = the_data['InteractionID']
+    user_timestamp = the_data['UserTimestamp']
+    content = the_data['Content']
+    status = the_data['Status']
+
+    # Constructing the query
+    #ModeratorId, StudentID, InteractionID, UserTimestamp, Content, Status
+    query = 'update User_Interaction set '
+
+    
+
+    query += 'ModeratorId = ' + str(moderator_id) + ', '
+    query += 'StudentID = ' + str(student_id) + ', ' 
+    query += 'InteractionID = ' + str(interaction_id) + ', ' 
+    query += 'UserTimestamp = ' + str(user_timestamp) + ', ' 
+    query += 'Content = ' + '"' + content + '", ' 
+    query += 'Status = ' + str(status) + '"'
+
+    query += 'where InteractionID = '
+    query += str(interaction_id)
+    query += ";"
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+
+@user_interactions.route('/user_interactions/interaction_id/<interaction_id>', methods=['DELETE'])
+def delete_guideline (interaction_id):
+
+    query = 'DELETE FROM Guidelines WHERE InteractionID = ' + str(interaction_id)
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
